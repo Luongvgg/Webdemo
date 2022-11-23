267b8ffea26d0b58cd3fabb039437d7970fa589e
Luongvgg <105184305+Luongvgg@users.noreply.github.com>
2022-11-23 20:49:40 +0700

    Add files via upload

---
+++ modules/left/list_pro.php
@@ -0,0 +1,34 @@
+
+<?php
+	include_once('admincp/modules/config.php');
+?>	
+        	<p class="loai">Loại</p>
+        	<ul>
+            <?php 
+				while($dong=mysql_fetch_array($run_loai)){
+			?>
+            	<li><a href="index.php?xem=loai&id=<?php echo $dong['loai_id']?>"><?php echo $dong['tenloai'] ?></a></li>
+   <?php
+				}
+   ?>                   
+          </ul>
+            <?php
+          $sgl2="select * from hieu";
+	$run_hieu=mysql_query($sgl2);
+           	?>
+			<p class="loai">Hiệu</p>
+        	<ul>
+            <?php
+			while($dong=mysql_fetch_array($run_hieu)){
+			?>
+            	<li><a href="index.php?xem=hieu&id=<?php echo $dong['hieu_id']?>"><?php echo $dong['tenhieu'] ?></a></li>
+               
+            <?php
+			}
+			?>
+            </ul>
+          
+          
+           
+           
+      
